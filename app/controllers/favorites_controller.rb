c6b82968e6716cdb56decfb34eaa3464e4867496
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-11-06 23:41:38 +0000

    [Fix]いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,11 +4,14 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    if request.referer&.include?("/books/#{book.id}")
-      redirect_to book_path(book.id)
-    else
-      redirect_to books_path
-    end
+    
+    # request.refererで直前のページに戻る
+    redirect_to request.referer
+    # if request.referer&.include?("/books/#{book.id}")
+    #   redirect_to book_path(book.id)
+    # else
+    #   redirect_to books_path
+    # end
   end
   
   
@@ -17,15 +20,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    if request.referer&.include?("/books/#{book.id}")
-      redirect_to book_path(book.id)
-    else
-      redirect_to books_path
-    end
+    redirect_to request.referer
+    # if request.referer&.include?("/books/#{book.id}")
+    #   redirect_to book_path(book.id)
+    # else
+    #   redirect_to books_path
+    # end
   end
   
-  private
-  def book_favorite_params
-    params.require(:favorite).permit(:favorite)
-  end
+#   private
+#   def book_favorite_params
+#     params.require(:favorite).permit(:favorite)
+#   end
 end
