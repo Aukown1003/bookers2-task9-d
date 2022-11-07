46f00a28b3eee602dbef436aab327f0a1cf6a8ed
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-11-07 10:55:06 +0000

    [Fix]method get_profile_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,10 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -17,9 +15,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
+    member do
+      get :following, :followers
+      # GET /users/id/following
+      # GET /users/id/followers
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
