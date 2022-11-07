c7f819708add243f1a0ae9039370e1d7c6952673
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-11-07 01:39:45 +0000

    [Add]relationshipsアソシエーション

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+  # class_name: "User"でUserモデルを参照
+  # belongs_to :userとするとどっちがどっちのuserかわからなくなる
+  # followerテーブルを探しに行ってしまうので、class_name: "User"でuserテーブルからデータをとってきてもらう
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,33 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :books, dependent: :destroy
+  #ここまでは一緒、class_name:でRelationshipテーブルを指定
+  # foreign_key(外侮キー)で参照するカラムを指定
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  # フォロー・フォロワーの一覧画面で、user.followersという記述でフォロワーを表示したい
+  # throughでスルーするテーブル、sourceで参照するカラムを指定
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
 
   
   def get_profile_image
